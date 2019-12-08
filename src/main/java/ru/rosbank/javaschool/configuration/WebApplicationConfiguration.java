ac343262b7bd6fe3e733ef66f165899fdb311225
KirVasilchenko <uzzer-13@mail.ru>
2019-12-08 05:22:05 +0300

    Optimized configuration files.

--- src/main/java/ru/rosbank/javaschool/configuration/WebApplicationConfiguration.java
+++ src/main/java/ru/rosbank/javaschool/configuration/WebApplicationConfiguration.java
@@ -1,10 +1,6 @@
 package ru.rosbank.javaschool.configuration;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -17,12 +13,4 @@ public class WebApplicationConfiguration implements WebMvcConfigurer {
         .allowedHeaders("*")
         .allowedMethods("*");
   }
-
-//  Это для Spring Security (поторопились)
-//  @Bean
-//  public CorsConfigurationSource corsConfigurationSource() {
-//    var source = new UrlBasedCorsConfigurationSource();
-//    source.registerCorsConfiguration("/**", new CorsConfiguration().applyPermitDefaultValues());
-//    return source;
-//  }
 }
