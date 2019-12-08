d7e5517ae32ab1974a7d3e5bc4d4e31b5168afc9
KirVasilchenko <uzzer-13@mail.ru>
2019-12-08 05:17:56 +0300

    Prepared data for testing.

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,4 +1,12 @@
 INSERT INTO posts (content)
-VALUES ('FIRST POST'),
-       ('SECOND POST'),
-       ('THIRD POST');
+VALUES ('POST 1'),
+       ('POST 2'),
+       ('POST 3'),
+       ('POST 4'),
+       ('POST 5'),
+       ('POST 6'),
+       ('POST 7'),
+       ('POST 8'),
+       ('POST 9'),
+       ('POST 10'),
+       ('SUPER POST 1');
